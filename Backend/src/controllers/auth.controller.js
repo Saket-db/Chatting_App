de22b0cad230fe7da4c4a4930b845bb219ba267a
Saket-db <dbsaket04@gmail.com>
2025-02-14 17:20:32 +0530

    ProfilePic

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 /**
  * @desc   User Signup
@@ -111,9 +112,31 @@ export const logout = (req, res) => {
 export const updateProfile = async (req, res) =>{
     try {
         const { profilePic } = req.body;
-        req.user._id
+        const usser = req.user._id
+
+        if(!profilePic)
+        {
+            return res.status(400).json({ message: "Please add a profile picture" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+        res.status(200).json(updatedUser)
     }
     catch(error){
-        
+        console.log("error in update profile:", error);
+        res.status(500).json({ message: "Server error" });
+
     }
+};
+
+export const checkAuth = (req,res) => {
+    try {
+        res.status(200).json(req.user);
+      } 
+      catch (error)
+      {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
 };
