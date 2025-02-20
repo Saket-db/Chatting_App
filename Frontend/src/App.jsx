113238a17b88a2e37d0593cc87a26a5d424e3ef6
Saket-db <dbsaket04@gmail.com>
2025-02-20 22:31:11 +0530

    Router_Created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import Navbar from './components/Navbar';
+
+import {Routes, Route} from 'react-router-dom';
+
 const App = () => {
   return (
-    <div className='text-red-500'>
-      Hello
+    <div >
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+
+      </Routes>
+
     </div>
   )
 }
