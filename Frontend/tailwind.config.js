da183b6b482ff076181839b8b9e7720f5c42ab48
Saket-db <dbsaket04@gmail.com>
2025-02-20 21:31:29 +0530

    Tailwind Integrated

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
