5f47ab17d2c31785fcb5481a310e7513653858d7
Saket-db <dbsaket04@gmail.com>
2025-03-24 15:51:52 +0530

    LoginPage

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthStore } from '../store/useAuthStore';
 
 const LoginPage = () => {
+    const [ShowPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+    });
+    const { login, isLoggingIn } = useAuthStore();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      login(formData);
+    }; 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <div>
       Login
