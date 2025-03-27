060677a4a78d53bfc4045bfa09e7c91681a1ad51
Saket-db <dbsaket04@gmail.com>
2025-03-27 20:37:47 +0530

    Sidebar_Progress_1

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
+import { Users } from 'lucide-react';
 
 const Sidebar = () => {
     const{getUsers, users, isUsersLoading, selectedUser, setSelectedUser,} = useChatStore()
@@ -15,9 +16,17 @@ const Sidebar = () => {
     if(isUsersLoading) return <SidebarSkeleton/>
 
   return (
-    <div>
-      
+   <aside className='h-full lg:w-72 border-r border-base-300 flex flex-col transition-all duration-150 sm:hidden'>
+    <div className='border-b border-base-300 w-full p-5'>
+      <div className='flex items-center gap-2'>
+        <Users className='size-7'/>
+        <span className='font-medium hidden lg:block'>Contacts</span>
+      </div>
+
+      <div className='overflow-y-auto w-full py-3'></div>
     </div>
+
+   </aside>
   )
 }
 
