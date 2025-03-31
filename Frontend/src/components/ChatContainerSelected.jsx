5168b9d1a2050a22094009c5abc3a85cf8c2e517
Saket-db <dbsaket04@gmail.com>
2025-03-31 16:27:01 +0530

    ChatHeader

--- Frontend/src/components/ChatContainerSelected.jsx
+++ Frontend/src/components/ChatContainerSelected.jsx
@@ -1,26 +1,63 @@
 import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
+import ChatHeader from './ChatHeader'
 // import { useEffect } from 'react'
 
 const ChatContainerSelected = () => {
 
-  const{messages, getMessages, isMessageLoading, selectdUser} = useChatStore()
+  const{messages, getMessages, isMessageLoading, selectedUser} = useChatStore()
 
   useEffect(() =>{
-    getMessages(selectdUser._id)
-  },[selectdUser._id, getMessages])
+    getMessages(selectedUser._id)
+  },[selectedUser._id, getMessages])
 
   if(isMessageLoading) return
   <div>Loading..</div>
 
   return (
-    <div className='flex-1 flex flex-col overflow-auto'>
-      <ChatHeader />
+    <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader />
 
-      <p>messages..</p>
-
-      <MessageInput/>
+    <div className="flex-1 overflow-y-auto p-4 space-y-4">
+      {messages.map((message) => (
+        <div
+          key={message._id}
+          className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          ref={messageEndRef}
+        >
+          <div className=" chat-image avatar">
+            <div className="size-10 rounded-full border">
+              <img
+                src={
+                  message.senderId === authUser._id
+                    ? authUser.profilePic || "https://res.cloudinary.com/dyy1u7wvc/image/upload/v1742831930/skyswnfq1cpwysmf7slp.png"
+                    : selectedUser.profilePic || "https://res.cloudinary.com/dyy1u7wvc/image/upload/v1742831930/skyswnfq1cpwysmf7slp.png"
+                }
+                alt="profile pic"
+              />
+            </div>
+          </div>
+          <div className="chat-header mb-1">
+            <time className="text-xs opacity-50 ml-1">
+              {formatMessageTime(message.createdAt)}
+            </time>
+          </div>
+          <div className="chat-bubble flex flex-col">
+            {message.image && (
+              <img
+                src={message.image}
+                alt="Attachment"
+                className="sm:max-w-[200px] rounded-md mb-2"
+              />
+            )}
+            {message.text && <p>{message.text}</p>}
+          </div>
+        </div>
+      ))}
     </div>
+
+    {/* <MessageInput /> */}
+  </div>
   )
 }
 
