0c4ca6366974a4fe44a8ad8fed81f12280e8df87
Saket-db <dbsaket04@gmail.com>
2025-04-08 22:51:34 +0530

    Revert "Fullstack complete"
    This reverts commit 952a210c360a604175209a1834802b70a61dfa67.

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { persist } from "zustand/middleware"; // ✅ Add persist
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/"
+
+const BASE_URL = "http://localhost:5002"
 
 export const useAuthStore = create(
   persist(
